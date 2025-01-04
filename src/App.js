fdb5104662b556a39f44a01835b5841443d26fb6
Itzhak Bokris <jzak300@gmail.com>
2025-01-04 23:22:52 +0200

    fix qr account issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import './App.css';
 
 const DYMENSION_CONNECT_URL = 'http://localhost:3000';
@@ -6,13 +6,16 @@ const DYMENSION_CONNECT_NETWORK_IDS = ['evmtestiroagain_283331-1'];
 const DYMENSION_CONNECT_NETWORK_MAIN_DENOM = 'aevm'
 
 function App() {
+    const [dymensionConnectOpen, setDymensionConnectOpen] = useState(false);
     const [dymensionConnectReady, setDymensionConnectReady] = useState(false);
     const [address, setAddress] = useState('');
     const [hexAddress, setHexAddress] = useState('');
     const buttonRef = useRef(null);
     const iframeRef = useRef(null);
     const [broadcasting, setBroadcasting] = useState(false);
-    const [menuVisible, setMenuVisible] = useState(false);
+    const [notifications, setNotifications] = useState([]);
+
+    const qrAccount = useMemo(() => new URLSearchParams(window.location.search).get('qrAccount'), []);
 
     const sendMessage = useCallback((message) => iframeRef.current?.contentWindow?.postMessage(message, DYMENSION_CONNECT_URL), []);
 
@@ -71,6 +74,9 @@ function App() {
             if (event.data.type === 'ready') {
                 setDymensionConnectReady(true);
             }
+            if (event.data.type === 'menu-visible') {
+                setDymensionConnectOpen(event.data.value);
+            }
             if (event.data.type === 'connect') {
                 setHexAddress(event.data.hexAddress);
                 setAddress(event.data.address);
@@ -81,15 +87,12 @@ function App() {
                 setAddress('');
                 updateTriggerBoundingRect();
             }
-            if (event.data.type === 'menu-visible') {
-                setMenuVisible(event.data.value);
-                if (event.data.value) {
-                    updateTriggerBoundingRect();
-                }
-            }
             if (event.data.type === 'tx-response') {
                 setBroadcasting(false);
                 setTimeout(() => alert(JSON.stringify(event.data.response) || event.data.error?.message), 50);
+            }
+            if (event.data.type === 'notification') {
+                setNotifications(event.data.messages);
             }
         }
         window.addEventListener('message', handleMessage);
@@ -102,23 +105,30 @@ function App() {
             <button
                 disabled={!dymensionConnectReady}
                 ref={buttonRef}
-                onClick={() => sendMessage({type: 'toggleMenu'})}
+                onClick={() => {
+                    setDymensionConnectOpen(!dymensionConnectOpen)
+                    updateTriggerBoundingRect();
+                }}
             >
                 {hexAddress || 'Connect'}
             </button>
             <iframe
                 ref={iframeRef}
                 onLoad={initModal}
+                style={{display: dymensionConnectOpen || qrAccount ? 'block' : 'none'}}
                 allow='clipboard-read; clipboard-write; camera'
                 title='dymension-connect'
                 className='dymension-connect-iframe'
-                style={{pointerEvents: menuVisible ? 'auto' : 'none'}}
-                src={`${DYMENSION_CONNECT_URL}/connect?networkIds=${DYMENSION_CONNECT_NETWORK_IDS.join(',')}`}
+                src={`${DYMENSION_CONNECT_URL}/connect${qrAccount ? `/account/${qrAccount}` : ''}?networkIds=${DYMENSION_CONNECT_NETWORK_IDS.join(',')}`}
             />
             <br />
             <button disabled={!address || broadcasting} onClick={sendToMyself}>
                 Send 1 token to myself {broadcasting ? '- broadcasting' : ''}
             </button>
+            <div className='snack-bar-messages'>
+                {notifications.map((notification) =>
+                    <div className='message' key={notification.key}>{notification.content}</div>)}
+            </div>
         </div>
     );
 }
